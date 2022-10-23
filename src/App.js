cb281f2138b0963926ee8a8d75af5ba3e51ec144
Семен Пурнемцев <purnemtzev.semen@yandex.ru>
2022-10-23 17:21:34 +0300

    delete useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Main from './Components/Main';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components/macro';
 import useSetColor from './Hooks/useSetColor';
 
@@ -18,11 +18,6 @@ const StyledApp = styled.div`
 
 function App() {
     const [date, setDate] = useState(new Date());
-    useEffect(() => {
-        document.body.style.background = `#${Math.floor(
-            Math.random() * 0xffffff
-        ).toString(16)}`;
-    }, []);
 
     let days = [
         'Воскресенье',
