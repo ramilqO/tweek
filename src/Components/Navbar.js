1bfeab12f220da053bd8d0c09ca1798f7f34c108
Семен Пурнемцев <purnemtzev.semen@yandex.ru>
2022-10-23 13:21:24 +0300

    add date

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,68 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {AiFillCaretLeft} from "react-icons/ai";
-import {AiFillCaretRight} from "react-icons/ai";
+import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
 
 const StyledNavbar = styled.div`
     width: 100%;
     height: 100%;
-`
+`;
 
 const StyledContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 30px;
-`
+`;
 
 const StyledDataHeader = styled.div`
     font-size: 36px;
     color: white;
     font-weight: 900;
     color: black;
-
-    &:hover {
-        cursor: pointer;
-    }
-`
-
-// const StyledNavButton = styled.button`
-//     width: 42px;
-//     height: 42px;
-//     background: black;
-//     color: white;
-
-//     border-radius: 50%;
-//     border: 0;
-
-//     margin: 10px;
-// `
+    cursor: pointer;
+`;
 
 const StyledNavContainer = styled.div`
     display: flex;
-`
+`;
 
-const Navbar = () => {
+const Navbar = ({ monthNames, date }) => {
     return (
         <StyledNavbar>
             <StyledContainer>
-                <StyledDataHeader>Месяц + Год</StyledDataHeader>
+                <StyledDataHeader>
+                    <p>
+                        {monthNames[date.getMonth()]} {date.getFullYear()}
+                    </p>
+                </StyledDataHeader>
 
                 <StyledNavContainer>
-
-                {/* <StyledNavButton><AiFillCaretLeft /></StyledNavButton>
-                <StyledNavButton>+</StyledNavButton> */}
-
-                <AiFillCaretLeft />
-                <AiFillCaretRight />
-                
-
+                    <AiFillCaretLeft />
+                    <AiFillCaretRight />
                 </StyledNavContainer>
-                
             </StyledContainer>
         </StyledNavbar>
     );
-}
+};
 
 export default Navbar;
